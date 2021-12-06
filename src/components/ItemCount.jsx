3b158dc78f2529ec9390d6eac65f75111374d8d2
nognzlz <n.gonzalezparra@globant.com>
2021-12-06 17:58:42 -0300

    feat: ItemCount

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,95 @@
+import { Paper, IconButton, Box, Typography, Button } from '@mui/material';
+import React, { useState } from 'react';
+import { makeStyles } from '@mui/styles';
+import { AddCircle, RemoveCircle } from '@mui/icons-material';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        padding: '0.5rem',
+    },
+    text: {
+        userSelect: 'none',
+    },
+}));
+
+const ItemCount = ({ stock, product, onAddFn, initial }) => {
+    const classes = useStyles();
+
+    const [quantity, setQuantity] = useState(initial || 0);
+
+    const addQuantity = (value) => {
+        const result = quantity + value;
+        if (result >= 0) {
+            setQuantity(result);
+        }
+    };
+
+    return (
+        <Box sx={{ width: 400 }}>
+            <Box
+                sx={{
+                    bgcolor: 'neutral.main',
+                    borderRadius: 1,
+                    padding: '1rem',
+                    marginBottom: '1rem',
+                }}
+            >
+                <Typography variant={'body1'} className={classes.text}>
+                    {product}
+                </Typography>
+                <Typography variant={'body1'} className={classes.text}>
+                    Stock disponible: {stock}
+                </Typography>
+
+                <Paper variant="outlined" className={classes.paper}>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <IconButton
+                            aria-label="remove"
+                            size="small"
+                            onClick={() => {
+                                addQuantity(-1);
+                            }}
+                            disabled={quantity < 1}
+                        >
+                            <RemoveCircle />
+                        </IconButton>
+                        <Typography className={classes.text}>
+                            {quantity}
+                        </Typography>
+                        <IconButton
+                            aria-label="add"
+                            size="small"
+                            onClick={() => {
+                                addQuantity(1);
+                            }}
+                            disabled={quantity >= stock}
+                        >
+                            <AddCircle />
+                        </IconButton>
+                    </Box>
+                </Paper>
+            </Box>
+            <Button
+                sx={{
+                    width: '100%',
+                }}
+                variant="outlined"
+                color="secondary"
+                disabled={quantity < 1 || quantity > stock}
+                onClick={() => {
+                    onAddFn(quantity);
+                }}
+            >
+                Agregar al carrito
+            </Button>
+        </Box>
+    );
+};
+
+export default ItemCount;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,23 @@
 import Container from '../UI/Container';
 import React from 'react';
 import ItemListContainer from './ItemListContainer';
+import ItemCount from './ItemCount';
 
 export const Main = () => {
+    const onAdd = (quantity) => {
+        alert(`Se agregaron ${quantity} productos a tu carrito`);
+    };
+
     return (
         <Container>
-            <ItemListContainer>Bienvenidos a Nico Dietetica!</ItemListContainer>
+            <ItemListContainer>
+                <ItemCount
+                    stock={3}
+                    product={'Miel orgánica'}
+                    onAddFn={onAdd}
+                    initial={1}
+                />
+            </ItemListContainer>
         </Container>
     );
 };
